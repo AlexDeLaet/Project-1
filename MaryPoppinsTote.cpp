f2cffc6a19b7696fb0068e9d4463b2c70b2d5501
lukejsnyder95 <43047226+lukejsnyder95@users.noreply.github.com>
2018-09-16 18:57:31 -0400

    Update MaryPoppinsTote.cpp

--- MaryPoppinsTote.cpp
+++ MaryPoppinsTote.cpp
@@ -1,60 +1,58 @@
+#pregma
 #include <iostream>
+#include MaryPoppinsTote.hpp
 using namespace std;
 
 class MarryPoppinsTote{
 
-public:
-int toteSize;
-//MarryPoppinsTote int *tote;
-int tote[];
-
-private:
 
 MarryPoppinsTote(){
+  toteSize = 0;
+  tote = new T[0];
+  
+
+//Copy Constructor: Copying data from original Tote to newTote
+MarryPoppinsTote(const MarryPoppinsTote& other){
   //Initialize
-
-  //check to see if array tote has any objects
-  if(tote == NULL){
-  tote = new int tote[1];
-  }
-  else{
-  //int newToteSize = toteSize;
-    MarryPoppinsTote* newTote = new MarryPoppinsTote[toteSize + 1];
+  (*this).tote = int[other.toteSize];
+  for (int i = 0; i < other.toteSize; i++) {
+      new tote[i] = other.tote[i];
   }
 
-//Copy Constructor: Coppying data from original Tote to newTote
-MarryPoppinsTote(const MarryPoppinsTote & other){
-  //Inizialize
-  this.tote = int[other.toteSize];
-  for (index i = 0; i < toteSize; i++) {
-      newTote[i] = tote[i];
-  }
-//  this.newTote = other.tote;
 
 }
 
-//Deconstructor to delete orignal tote array
+//Deconstructor to delete original tote array
 ~MarryPoppinsTote(){
-  delete[] tote;
+  if(tote){
+    delete(tote);
+    }
   }
 
+//Place takes a item and places it into the tote
 void place(int item){
-
+  toteSize = toteSize + 1;
+  tote = new int[toteSize];
+  item = tote[tote.size];
 
   }
+
+
+
+
 
 int search(int item){
   int count = 0;
-  for(int i = 0; i<tote.size; i++ ){
-      if(item == tote[i]){
-        count = count +1;  
-      }
-
-    }
-    return count;
+   for(int i = 0; i<tote.size; i++ ){
+       if(item == tote[i]){
+         count = count +1;  
+       }
+ 
+     }
+     return count;
 
   }
-
+// Randomly searches the tote array, if there are no items within tote throw exception
 int reach(){
   int randomNumber;
   if(tote == NULL){
@@ -65,6 +63,22 @@ int reach(){
   }
 }
 
+
 MarryPoppinsTote& operator=(const MarryPoppinsTote & other){
+  if(other >= 0 && other < toteSize){
+            return tote[other];
+        }
+        else{
+            throw(1);
+        }
+    }
 
   }
+
+
+
+
+};// end class
+
+
+
